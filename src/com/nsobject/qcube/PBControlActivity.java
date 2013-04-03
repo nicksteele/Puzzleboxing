9c03d63932eac0aca19f423ce40d47b8c658d7c7
Nick Steele <nickelsteele@gmail.com>
2013-04-03 15:20:54 -0400

    layout

--- src/com/nsobject/qcube/PBControlActivity.java
+++ src/com/nsobject/qcube/PBControlActivity.java
@@ -1,11 +1,58 @@
 package com.nsobject.qcube;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.media.SoundPool.OnLoadCompleteListener;
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+
+import android.annotation.SuppressLint;
+
 import android.app.Activity;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+
+import android.graphics.Color;
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
+
+import android.util.Log;
+
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
 
-public class PBControlActivity extends Activity {
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.text.method.ScrollingMovementMethod;
+import android.media.AudioManager;
 
+public class PBControlActivity extends Activity implements SeekBar.OnSeekBarChangeListener {
+
+	
+	//Config
+	Configuration config;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,5 +65,23 @@ public class PBControlActivity extends Activity {
 		getMenuInflater().inflate(R.menu.pbcontrol, menu);
 		return true;
 	}
+	
+	public void onStartTrackingTouch(SeekBar seekBar){
+		
+	}
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
